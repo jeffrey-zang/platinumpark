b48e5b7171e1ca73096d632ab42faf4d89bbed39
Li Feng Yin <lifeng-yin@proton.me>
2023-09-23 14:56:48 -0400

    feat(front): add create page

--- src/pages/Create/Create.tsx
+++ src/pages/Create/Create.tsx
@@ -1,6 +1,66 @@
+import { useState } from "react"
+import { BiSolidCar, BiSolidCommentDetail } from "react-icons/bi"
+import './Create.scss'
+
 const Create = () => {
+  const [newLot, setNewLot] = useState({
+    name: '',
+    address: '',
+    spots: 0,
+    imageUrl: '',
+  })
+
+  const updateNewLot = (name: string, value: any) => {
+    console.log(name, value)
+    setNewLot(lot => ({ ...lot, [name]: value}))
+  }
+
+  const handleClick = () => {
+    setNewLot({
+      name: '',
+      address: '',
+      spots: 0,
+      imageUrl: '',
+    })
+  }
+
   return (
-    <div>Create</div>
+    <div className='p-12 all w-[80vw] bg-[#fafafa]'>
+      <h1>Create lot</h1>
+      <p>Create a lot.</p>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-full gap-x-8 gap-y-2 mt-4'>
+
+        <div className="input-field">
+          <label htmlFor="name">Name</label>
+          <input
+            name='name'
+            value={newLot.name}
+            onChange={e => updateNewLot('name', e.target.value)}
+            placeholder="Parking Lot A"
+          />
+        </div>
+
+        <div className="input-field">
+          <label htmlFor="address">Address</label>
+          <input
+            name='address'
+            value={newLot.address}
+            onChange={e => updateNewLot('address', e.target.value)}
+            placeholder="200 University Ave W"
+          />
+        </div>
+        <div className="input-field">
+          <label htmlFor="image">Image URL</label>
+            <input
+              name="image"
+              value={newLot.imageUrl}
+              onChange={e => updateNewLot('imageUrl', e.target.value)}
+              placeholder="https://imageurl.com/parking-lot-a"
+            />
+          </div>
+        </div>
+        <button className="mt-4" onClick={handleClick}>Create lot</button>
+      </div>
   )
 }
 
